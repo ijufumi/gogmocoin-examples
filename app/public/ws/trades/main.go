b784bd6a07a23cc2de662e30b0ca433a2f27af32
Takafumi Iju <ijufumi@gmail.com>
2024-07-31 07:55:13 +0900

    feat: Updated gogmocoin version

--- app/public/ws/trades/main.go
+++ app/public/ws/trades/main.go
@@ -2,18 +2,22 @@ package main
 
 import (
 	"github.com/ijufumi/gogmocoin/v2/api/common/consts"
-	"github.com/ijufumi/gogmocoin/v2/api/public/ws/channel/trades"
+	"github.com/ijufumi/gogmocoin/v2/api/public/ws"
 	"log"
 	"time"
 )
 
 func main() {
-	client := trades.New(consts.SymbolBTCJPY, nil)
+	client := ws.NewTrades(consts.SymbolBTCJPY, nil)
 	timeoutCnt := 0
+	err := client.Subscribe()
+	if err != nil {
+		log.Fatalln(err)
+	}
 	for {
 		select {
 		case v := <-client.Receive():
-			log.Printf("msg:%+v", v)
+			log.Printf("msg:%+v\n", v)
 		case <-time.After(180 * time.Second):
 			log.Println("timeout...")
 			timeoutCnt++
--- go.mod
+++ go.mod
@@ -3,11 +3,9 @@ module github.com/ijufumi/gogmocoin-examples
 go 1.21
 
 require (
+	github.com/ijufumi/gogmocoin/v2 v2.2.0
 	github.com/joho/godotenv v1.5.1
 	github.com/shopspring/decimal v1.4.0
-	github.com/ijufumi/gogmocoin/v2 v2.1.1
 )
 
-require (
-	github.com/gorilla/websocket v1.5.3 // indirect
-)
+require github.com/gorilla/websocket v1.5.3 // indirect
